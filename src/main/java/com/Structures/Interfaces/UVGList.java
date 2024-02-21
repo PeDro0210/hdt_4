c86087ba8aa1b1a31a2c20561e313e28d0f1f5ce
Diego L <lop23747@uvg.edu.gt>
2024-02-21 06:40:14 -0600

    Refactor data structures interfaces and classes, doc in each file too

--- src/main/java/com/Structures/Interfaces/UVGList.java
+++ src/main/java/com/Structures/Interfaces/UVGList.java
@@ -1,7 +1,19 @@
 package com.Structures.Interfaces;
 
+/**
+ * Interfaz que define las operaciones básicas de una lista.
+ * @param <T> el tipo de elementos que contendrá la lista.
+ */
 public interface UVGList<T> {
+    /**
+     * Agrega un elemento a la lista.
+     * @param value el valor del elemento a agregar.
+     */
     void add(T value);
-    T removeLast();
 
+    /**
+     * Remueve y devuelve el último elemento de la lista.
+     * @return el último elemento de la lista.
+     */
+    T removeLast();
 }
